1f732a9b392533f0060e9f671d6c96580eb76224
dhythm <yuta.okada.20@gmail.com>
2024-11-23 22:11:27 +0900

    feat: implement Steps and ConditionStep

--- src/components/ConditionStep.tsx
+++ src/components/ConditionStep.tsx
@@ -1,3 +1,4 @@
+import styled from "@emotion/styled";
 import { HorizontalContainer } from "./HorizontalContainer";
 import { HorizontalLine } from "./HorizontalLine";
 import { SharpCurve } from "./SharpCurve";
@@ -17,12 +18,23 @@ export const ConditionStep: React.FC<{
         </HorizontalContainer>
         <VerticalLine />
         <>foo</>
+        <VerticalLine />
+        <BottomLine />
       </VerticalContainer>
       <VerticalContainer>
         <SharpCurve />
         <VerticalLine />
         <>bar</>
+        <VerticalLine />
       </VerticalContainer>
     </HorizontalContainer>
   );
 };
+
+const BottomLine = styled.div<{ color?: string }>`
+  position: relative;
+  right: 0;
+  left: 21px;
+  height: 2px;
+  background-color: #ffffff;
+`;
