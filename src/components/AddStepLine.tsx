e93b28c91144ea008fddfc5154697c3bb463be56
dhythm <yuta.okada.20@gmail.com>
2024-11-24 15:35:49 +0900

    implement Modal to add a step

--- src/components/AddStepLine.tsx
+++ src/components/AddStepLine.tsx
@@ -4,35 +4,80 @@ import styled from "@emotion/styled";
 import { BLOCK_WIDTH } from "./utils/constants";
 import { MdAddCircleOutline } from "react-icons/md";
 import { useState } from "react";
+import Modal from "react-modal";
 
 export const AddStepLine: React.FC<{ onClick?: () => void }> = ({
   onClick,
 }) => {
   const [isHovering, setIsHovering] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <VerticalLineWrapper
-      onMouseEnter={() => {
-        setIsHovering(true);
-      }}
-      onMouseLeave={() => {
-        setIsHovering(false);
-      }}
-    >
-      {/*
-       * onMouseLeave will be fired when the icon is shown.
-       * This will make infinite rendering.
-       */}
-      <VerticalLine />
-      {isHovering && (
-        <Clickable onClick={onClick}>
-          <IconWrapper>
-            <MdAddCircleOutline color="#000000" />
-          </IconWrapper>
-        </Clickable>
-      )}
-    </VerticalLineWrapper>
+    <>
+      <VerticalLineWrapper
+        onMouseEnter={() => {
+          setIsHovering(true);
+        }}
+        onMouseLeave={() => {
+          setIsHovering(false);
+        }}
+      >
+        {/*
+         * onMouseLeave will be fired when the icon is shown.
+         * This will make infinite rendering.
+         */}
+        <VerticalLine />
+        {isHovering && (
+          <Clickable
+            onClick={() => {
+              onClick?.();
+              setIsOpen(true);
+            }}
+          >
+            <IconWrapper>
+              <MdAddCircleOutline color="#000000" />
+            </IconWrapper>
+          </Clickable>
+        )}
+      </VerticalLineWrapper>
+
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={() => setIsOpen(false)}
+        style={customStyles}
+      >
+        <form>
+          <fieldset>
+            <legend>Add Operation or Condition Step.</legend>
+            <div>
+              <input type="radio" id="operation" name="stepType" checked />
+              <label htmlFor="operation">Operation</label>
+            </div>
+            <div>
+              <input type="radio" id="condition" name="stepType" />
+              <label htmlFor="condition">Condition</label>
+            </div>
+          </fieldset>
+          <ButtonWrapper>
+            <button onClick={() => setIsOpen(false)}>Cancel</button>
+            <button type="submit">Submit</button>
+          </ButtonWrapper>
+        </form>
+      </Modal>
+    </>
   );
+};
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    minWidth: "40%",
+  },
 };
 
 const IconWrapper = styled.span`
@@ -47,4 +92,10 @@ const VerticalLineWrapper = styled.div`
   width: ${BLOCK_WIDTH + 2}px;
   flex: 1;
   flex-direction: column;
+`;
+
+const ButtonWrapper = styled.div`
+  margin-top: 8px;
+  display: flex;
+  gap: 8px;
 `;
